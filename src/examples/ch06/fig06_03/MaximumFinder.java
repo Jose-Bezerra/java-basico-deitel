9e2a4a076f9b17f66f490a4f4e989be7025a18b3
José Bezerra <josbezfi@gmail.com>
2021-08-20 17:17:22 -0300

    Refactored file java Maximumfinder.

--- src/examples/ch06/fig06_03/MaximumFinder.java
+++ src/examples/ch06/fig06_03/MaximumFinder.java
@@ -1,10 +1,13 @@
 package examples.ch06.fig06_03;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MaximumFinder {
 
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter three floating-point values " +
@@ -15,8 +18,14 @@ public class MaximumFinder {
         double number3 = input.nextDouble();
 
         double result = maximum(number1, number2, number3);
+        double resultRefactored = maximumRefactored(number1, number2, number3);
 
         System.out.println("Maximum is: " + result);
+        System.out.println("Maximum Refactored is: " + resultRefactored);
+    }
+
+    private static double maximumRefactored(double number1, double number2, double number3) {
+        return Math.max(number1, Math.max(number2, number3));
     }
 
     private static double maximum(double x, double y, double z) {
